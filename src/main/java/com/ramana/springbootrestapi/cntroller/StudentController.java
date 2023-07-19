d47812cc01e7b1da3c32e2b93e4a1964f5e7cb54
Pade Ramanaiah <ramanaiah.pade111@outlook.com>
2023-07-19 20:23:17 +0530

    Spring Boot Rest Project

--- src/main/java/com/ramana/springbootrestapi/cntroller/StudentController.java
+++ src/main/java/com/ramana/springbootrestapi/cntroller/StudentController.java
@@ -3,6 +3,7 @@ package com.ramana.springbootrestapi.cntroller;
 import com.ramana.springbootrestapi.bean.Student;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,5 +26,16 @@ public class StudentController {
                 new Student(2, "Pade", "Ravi"),
                 new Student(4, "Y", "Lavanya")
         );
+    }
+
+    @GetMapping("/student/{id}")
+    public Student studentPathVariable(@PathVariable("id") int studentId) {
+        return List.of(
+                        new Student(1, "Pade", "ramanaiah"),
+                        new Student(2, "Pade", "Ravi"),
+                        new Student(4, "Y", "Lavanya"))
+                .stream()
+                .filter(student -> student.getId() == studentId)
+                .findAny().get();
     }
 }
